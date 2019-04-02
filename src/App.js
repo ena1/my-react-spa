26c7b4c66199303a40e3e7b9a9234ca7cbe18201
Ena Anicama <ena.anicama@gmail.com>
2019-04-02 02:48:23 -0400

    Initial commit - Install Bootstrap & dependencies: jquery, popper.js; react-icons, Reach Router, Firebase; create new components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Router, navigate } from "@reach/router";
+import firebase from "./Firebase";
+
+import Home from "./Home";
+import Welcome from "./Welcome";
+import Navigation from "./Navigation";
+import Login from "./Login";
+import Register from "./Register";
+import Meetings from "./Meetings";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      user: null,
+      displayName: null,
+      userID: null
+    };
+  }
+
+  componentDidMount() {
+    const ref = firebase.database().ref("user");
+
+    ref.on("value", snapshot => {
+      let FBUser = snapshot.val();
+      this.setState({ user: FBUser });
+    });
+  }
+
+  registerUser = userName => {
+    firebase.auth().onAuthStateChanged(FBUser => {
+      FBUser.updateProfile({
+        displayName: userName
+      }).then(() => {
+        this.setState({
+          user: FBUser,
+          displayName: FBUser.displayName,
+          userID: FBUser.uid
+        });
+        navigate("/meetings");
+      });
+    });
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Navigation user={this.state.user} />
+        {this.state.user && <Welcome user={this.state.displayName} />}
+
+        <Router>
+          <Home path="/" user={this.state.user} />
+          <Login path="/login" />
+          <Meetings path="/meetings" />
+          <Register path="/register" registerUser={this.registerUser} />
+        </Router>
       </div>
     );
   }
