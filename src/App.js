50fc411ee979abf3f46db980e14b5fa13553d0cc
ena1 <ena.anicama@gmail.com>
2019-04-24 03:31:13 -0400

    Edit App.js & Welcome.js: display registration info from database

--- src/App.js
+++ src/App.js
@@ -20,11 +20,14 @@ class App extends Component {
   }
 
   componentDidMount() {
-    const ref = firebase.database().ref("user");
-
-    ref.on("value", snapshot => {
-      let FBUser = snapshot.val();
-      this.setState({ user: FBUser });
+    firebase.auth().onAuthStateChanged(FBUser => {
+      if (FBUser) {
+        this.setState({
+          user: FBUser,
+          displayName: FBUser.displayName,
+          userID: FBUser.uid
+        });
+      }
     });
   }
 
@@ -47,7 +50,7 @@ class App extends Component {
     return (
       <div>
         <Navigation user={this.state.user} />
-        {this.state.user && <Welcome user={this.state.displayName} />}
+        {this.state.user && <Welcome userName={this.state.displayName} />}
 
         <Router>
           <Home path="/" user={this.state.user} />
--- src/Welcome.js
+++ src/Welcome.js
@@ -4,12 +4,12 @@ class Welcome extends Component {
   // constructor() {}
 
   render() {
-    const { user } = this.props;
+    const { userName } = this.props;
 
     return (
       <div className="text-center mt-4">
         <span className="text-secondary font-weight-bold pl-1">
-          Welcome {user}
+          Welcome {userName}
         </span>
         ,
         <a href="/" className="font-weight-bold text-primary pl-1">
